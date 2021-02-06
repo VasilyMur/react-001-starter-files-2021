0de6ccc939816ae1416c9b20f421ecd00acf620f
vasilymur <vmouravi@gmail.com>
2021-02-06 13:57:34 +0300

    initial commit

---
+++ src/sample-restaurants.js
@@ -0,0 +1,21 @@
+// Наши рестораны
+const restaurants = [
+  {
+    title: 'Hot Burger Тверская',
+    url: 'hot-burger-tverskaya',
+    id: 1
+  },
+
+  {
+    title: 'Hot Burger Маросейка',
+    url: 'hot-burger-maroseyka',
+    id: 2
+  },
+  {
+    title: 'Hot Burger Петровка',
+    url: 'hot-burger-petrovka',
+    id: 3
+  }
+];
+
+export default restaurants;
